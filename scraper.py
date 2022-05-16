773c92f55b80ca7608d71d0c8ff2504a0d5188e8
hokou <hokou.shiau@gmail.com>
2022-05-16 10:03:55 +0800

    3.3-批次爬取靜態網頁的資料

---
+++ scraper.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.bbc.com/zhongwen/trad/topics/c83plve5vmjt"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "lxml")
+titles = soup.find_all("span", {"class": "lx-stream-post__header-text gs-u-align-middle"})
+
+title_list = []
+for title in titles:
+    title_list.append(title.getText())
+
+print(title_list)
